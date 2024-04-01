e0fc34afe7a27da86275f8f123f8a6e8f56240cd
david.pits <david.pits@icp.es>
2024-04-01 10:27:05 +0200

    Añadida lógica de conexión entre app y DTM

--- app/src/main/java/es/icp/goodlibrary/MainActivity.kt
+++ app/src/main/java/es/icp/goodlibrary/MainActivity.kt
@@ -13,41 +13,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recibirDatosDTM()
+//        recibirDatosDTM()
     }
 
     //Recibimos los datos que nos devuelve DTM
-    private fun recibirDatosDTM() {
-        val dataResponse = intent.data
-
-        if (dataResponse != null) {
-            if (intent.extras != null) {
-                val datosIntent = intent.extras!!.getString("jsonDefaultResponse")
-                val datos = Gson().fromJson(datosIntent, DefaultResponse::class.java)
-                Log.d("DATOS RECIBIDOS: ", datos.toString())
-                enviarDatosApp(datos)
-            }
-        }
-    }
-
-    //Después de recibir los datos, enviamos los datos a la app que nos ha llamado
-    private fun enviarDatosApp(dates: DefaultResponse) {
-        val intent = Intent(Intent.ACTION_VIEW).apply {
-
-            //Depende del id_app que nos devuelva DTM, enviamos los datos a una app u otra
-            // De momento, enviamos a la misma app de pruebas
-            when (dates.id_app) {
-                1 -> data = Uri.parse("respuestaapp://result")
-                2 -> data = Uri.parse("respuestaapp://result")
-                3 -> data = Uri.parse("respuestaapp://result")
-                4 -> data = Uri.parse("respuestaapp://result")
-            }
-
-            putExtra("jsonDefaultRequest", Gson().toJson(dates))
-            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        }
-
-        this.startActivity(intent)
-    }
+//    private fun recibirDatosDTM() {
+//        val dataResponse = intent.data
+//
+//        if (dataResponse != null) {
+//            if (intent.extras != null) {
+//                val datosIntent = intent.extras!!.getString("jsonDefaultResponse")
+//                val datos = Gson().fromJson(datosIntent, DefaultResponse::class.java)
+//                Log.d("DATOS RECIBIDOS: ", datos.toString())
+//                enviarDatosApp(datos)
+//            }
+//        }
+//    }
+//
+//    //Después de recibir los datos, enviamos los datos a la app que nos ha llamado
+//    private fun enviarDatosApp(dates: DefaultResponse) {
+//        val intent = Intent(Intent.ACTION_VIEW).apply {
+//
+//            //Depende del id_app que nos devuelva DTM, enviamos los datos a una app u otra
+//            // De momento, enviamos a la misma app de pruebas
+//            when (dates.id_app) {
+//                1 -> data = Uri.parse("respuestaapp://result")
+//                2 -> data = Uri.parse("respuestaapp://result")
+//                3 -> data = Uri.parse("respuestaapp://result")
+//                4 -> data = Uri.parse("respuestaapp://result")
+//            }
+//
+//            putExtra("jsonDefaultRequest", Gson().toJson(dates))
+//            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+//        }
+//
+//        this.startActivity(intent)
+//    }
 
 }
