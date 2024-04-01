445265174a847b72b424587d37d935a44b3843ef
david.pits <david.pits@icp.es>
2024-04-01 12:49:42 +0200

    Cambiado parametro a String para que venga un json y no un objeto

--- prueba/build.gradle.kts
+++ prueba/build.gradle.kts
@@ -41,7 +41,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.davidpruebas00.GoodLibrary"
                 artifactId = "test"
-                version = "1.0.7"
+                version = "1.0.8"
             }
             repositories {
                 mavenLocal()
